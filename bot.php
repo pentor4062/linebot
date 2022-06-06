4e27c3a5b98107a16b6f6dd9212c3b0ecbf32291
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-06 16:17:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,10 +21,11 @@ if ( sizeof($request_array['events']) > 0 ) {
         $reply_token = $event['replyToken'];
 
         $text = $event['message']['text'];
+        $text2 = 'test';
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-//             'messages' => [['type' => 'text', 'text' => $text ]]
+//             'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+            'messages' => [['type' => 'text', 'text' => $text.$text2 ]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
