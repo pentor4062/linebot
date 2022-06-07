786c82e9882dfed775aa94f259cbb35e0153612f
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-07 14:23:32 +0700

    Create bot2.php

---
+++ bot2.php
@@ -0,0 +1,45 @@
+<?php
+
+
+// $API_URL = 'https://api.line.me/v2/bot/message';
+// $ACCESS_TOKEN = ''; 
+// $channelSecret = '';
+// $ACCESS_TOKEN = 'iEP1m9iqn55lr2lypAvXsoLTp1JqemCMEipBb4dBlZTn7uhE1Tufk7AAAmMJ/lsU5wvuaYZzo1VKY/n0TTKBGd0ESR7upDhFvDCS13bQVg4/rKnRmLl0GHKxCPzUNfR12yET2j+opyYRafunBP1JigdB04t89/1O/w1cDnyilFU='; 
+// $channelSecret = '828a59a440c39ae2662c63a0a5de0eef';
+
+// Access Token
+$access_token = 'iEP1m9iqn55lr2lypAvXsoLTp1JqemCMEipBb4dBlZTn7uhE1Tufk7AAAmMJ/lsU5wvuaYZzo1VKY/n0TTKBGd0ESR7upDhFvDCS13bQVg4/rKnRmLl0GHKxCPzUNfR12yET2j+opyYRafunBP1JigdB04t89/1O/w1cDnyilFU=';
+// รับค่าที่ส่งมา
+$content = file_get_contents('php://input');
+// แปลงเป็น JSON
+$events = json_decode($content, true);
+if (!empty($events['events'])) {
+    foreach ($events['events'] as $event) {
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // ข้อความที่ส่งกลับ มาจาก ข้อความที่ส่งมา
+            // ร่วมกับ USER ID ของไลน์ที่เราต้องการใช้ในการตอบกลับ
+            $messages = array(
+                'type' => 'text',
+                'text' => 'Reply message : '.$event['message']['text']."\nUser ID : ".$event['source']['userId'],
+            );
+            $post = json_encode(array(
+                'replyToken' => $event['replyToken'],
+                'messages' => array($messages),
+            ));
+            // URL ของบริการ Replies สำหรับการตอบกลับด้วยข้อความอัตโนมัติ
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer '.$access_token);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+            echo $result;
+        }
+    }
+}
+
+?>
