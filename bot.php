4b4afa7a6afe6d0c366fa04428363bff99e216a4
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-07 08:28:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'iEP1m9iqn55lr2lypAvXsoLTp1JqemCMEipBb4dBlZTn7uhE1Tufk7AAAmMJ/lsU5wvuaYZzo1VKY/n0TTKBGd0ESR7upDhFvDCS13bQVg4/rKnRmLl0GHKxCPzUNfR12yET2j+opyYRafunBP1JigdB04t89/1O/w1cDnyilFU='; 
+$ACCESS_TOKEN = 'vojBBXzJTjpRI6j8hsYK2jNSlRbLQ/A9FeRKn0S/1UjxGXkmyGt7PrU1/dUviP9+5wvuaYZzo1VKY/n0TTKBGd0ESR7upDhFvDCS13bQVg6rQRVGSy/euqNQG9pcrxq5hx4CH0J/PxpNeKsuDD4fwgdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = '828a59a440c39ae2662c63a0a5de0eef';
 
 
