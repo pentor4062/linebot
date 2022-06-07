edc43212c0a81d3bce9b0f86fabfde1693facb6a
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-07 14:35:53 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -6,7 +6,7 @@ $access_token = 'iEP1m9iqn55lr2lypAvXsoLTp1JqemCMEipBb4dBlZTn7uhE1Tufk7AAAmMJ/ls
 $content = file_get_contents('php://input');
 // แปลงเป็น JSON
 $events = json_decode($content, true);
-echo 'oOk';
+echo 'Ok';
 if (!empty($events['events'])) {
     foreach ($events['events'] as $event) {
         if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
@@ -14,7 +14,7 @@ if (!empty($events['events'])) {
             // ร่วมกับ USER ID ของไลน์ที่เราต้องการใช้ในการตอบกลับ
             $messages = array(
                 'type' => 'text',
-                'text' => 'Reply message : '.$event['message']['text']."\nUser ID : ".$event['source']['userId']."\nother : ".implode(" ",$event['source']),
+                'text' => 'Reply message : '.$event['message']['text']."\nUser ID : ".$event['source']['userId']."\nother : ".print_r($event),
                 
             );
             $post = json_encode(array(
