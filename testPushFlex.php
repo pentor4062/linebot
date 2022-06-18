ce5284988b45942cf879d5de3a203c1e5e2f03f4
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-18 15:40:05 +0700

    Update testPushFlex.php

--- testPushFlex.php
+++ testPushFlex.php
@@ -184,8 +184,8 @@
 	}';
   $flexDataJsonDeCode = json_decode($flexDataJson,true);
   $datas['url'] = "https://api.line.me/v2/bot/message/push";
-  $datas['token'] = "<access token>";
-  $messages['to'] = "<user id>";
+  $datas['token'] = "x8IjU+aePqcU5RUBRJlyvR1knBI8gkMfT2yharvm1iA1CH6ALXVskcekhf2AP3w65wvuaYZzo1VKY/n0TTKBGd0ESR7upDhFvDCS13bQVg74IljowJoh8ORV3PuNmfWEyh2ZTbg1EvGKhMgkaI/tpAdB04t89/1O/w1cDnyilFU=";
+  $messages['to'] = "U2689c41e6a80c9420c5447acbd6e730b";
   $messages['messages'][] = $flexDataJsonDeCode;
   $encodeJson = json_encode($messages);
 
